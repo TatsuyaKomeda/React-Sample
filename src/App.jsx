ada47907b42a746d5dc1e484c2dcd2a93cdf405b
Tatsuya.Komeda <98158024+TatsuyaKomeda@users.noreply.github.com>
2022-02-14 13:51:25 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num, faceShowFlag]);
+
+  return (
+    <React.Fragment>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>^_^</p>}
+    </React.Fragment>
+  );
+};
+
+export default App;
+
+// 10行目 <ColorfulMessage color="blue" message="お元気ですか？" />
+// {prop.message}
